79a15b6408341404da24950b11ed38869d0fef7c
YadavPrayank <prayank2456@gmail.com>
2026-07-07 15:27:39 +0530

    added PWA support

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'dairy-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.chunk.js',
+  '/static/js/bundle.js',
+  '/static/css/main.chunk.css',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});

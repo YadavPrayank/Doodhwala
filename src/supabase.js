2a80666d35b1fe5d9bf9c700f202d83c9c048dfe
YadavPrayank <prayank2456@gmail.com>
2026-07-10 11:30:57 +0530

    debug supabase

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
+if (!supabaseUrl || !supabaseKey) {
+  alert('Supabase credentials missing!')
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
